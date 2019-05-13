ffadab4456a9d7fc144a0c65ae10d765c7885972
Gabrieltmi <gabrieltameomaia@gmail.com>
2019-05-13 17:37:45 -0300

    Update

---
+++ FatehpurOverflowProject/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorHandler : MonoBehaviour
+{
+
+	public int sceneToLoad;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				SceneManager.LoadScene(sceneToLoad);
+			}
+		}
+	}
+}
---
+++ FatehpurOverflowProject/Assets/Scripts/coletavel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coletavel : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+		this.transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+    }
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+}
